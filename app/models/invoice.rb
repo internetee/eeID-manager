2f536742a78fd846f091e2cfde7b16d10e4201e1
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-03-17 15:39:04 +0200

    fix problem with autoloder

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,6 @@
 require 'countries'
 
 class Invoice < ApplicationRecord
-  include Invoice::BookKeeping
-
   alias_attribute :country_code, :alpha_two_country_code
   enum status: { issued: 'issued', paid: 'paid', cancelled: 'cancelled' }
   belongs_to :user, optional: true
@@ -131,5 +129,31 @@ class Invoice < ApplicationRecord
 
     BillingMailer.account_credited(user, self).deliver_later
     user.unsuspend_services!
+  end
+
+  def as_directo_json
+    invoice = ActiveSupport::JSON.decode(ActiveSupport::JSON.encode(self))
+    invoice['customer'] = compose_directo_customer
+    invoice['issue_date'] = issue_date.strftime('%Y-%m-%d')
+    invoice['transaction_date'] = paid_at&.strftime('%Y-%m-%d')
+    invoice['language'] = user.locale
+    invoice['invoice_lines'] = compose_directo_product
+
+    invoice
+  end
+
+  def compose_directo_product
+    [{ 'product_id' => 'ETTEM06', 'description' => "Order nr. #{number}",
+      'quantity' => 1, 'price' => ActionController::Base.helpers.number_with_precision(
+        price.to_s, precision: 2, separator: '.'
+      ) }].as_json
+  end
+
+  def compose_directo_customer
+    {
+      'code' => '',
+      'destination' => user.billing_alpha_two_country_code,
+      'vat_reg_no' => user.billing_vat_code
+    }.as_json
   end
 end
--- app/services/eis_billing/base.rb
+++ app/services/eis_billing/base.rb
@@ -1,24 +1,7 @@
 module EisBilling
   class Base
-    BASE_URL = ''
-    if Rails.env.staging?
-      BASE_URL = EidManager::Application.config
-                                             .customization[:billing_system_integration]
-                                             &.compact&.fetch(:eis_billing_system_base_url_staging, '')
-    else
-      BASE_URL = EidManager::Application.config
-                                             .customization[:billing_system_integration]
-                                             &.compact&.fetch(:eis_billing_system_base_url_dev, '')
-    end
-
+    BASE_URL = EidManager::Application.config.customization[:billing_system_integration]&.compact&.fetch(:eis_billing_system_base_url, '')
     INITIATOR = 'eeid'.freeze
-
-    SECRET_WORD = EidManager::Application.config
-                                              .customization[:billing_system_integration]
-                                              &.compact&.fetch(:secret_word, '')
-    SECRET_ACCESS_WORD = EidManager::Application.config
-                                                     .customization[:billing_system_integration]
-                                                     &.compact&.fetch(:secret_access_word, '')
 
     def self.base_request(url:)
       uri = URI(url)
@@ -33,11 +16,11 @@ module EisBilling
     end
 
     def self.generate_token
-      JWT.encode(payload, SECRET_WORD )
+      JWT.encode(payload, billing_secret )
     end
 
     def self.payload
-      { data: SECRET_ACCESS_WORD }
+      { initiator: INITIATOR }
     end
 
     def self.headers
@@ -45,6 +28,10 @@ module EisBilling
       'Authorization' => "Bearer #{generate_token}",
       'Content-Type' => 'application/json'
       }
+    end
+
+    def self.billing_secret
+      EidManager::Application.config.customization[:billing_system_integration]&.compact&.fetch(:billing_secret, '')
     end
   end
 end
