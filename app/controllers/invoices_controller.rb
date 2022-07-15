c69ca67067e0a23dbc353366a0355191fb001e48
Sergei Tsõganov <sergeit6@gmail.com>
2022-07-15 09:16:12 +0300

    Updated invoices controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,7 @@
 class InvoicesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_invoice, only: %i[create]
+  before_action :find_invoice, only: %i[show download]
   # GET /invoices
   def index
     @transactions = current_user.transactions.order(created_at: :desc)
@@ -10,13 +12,11 @@ class InvoicesController < ApplicationController
 
   # rubocop:disable Metrics/MethodLength
   def create
-    amount =  Money.from_amount(params[:amount].to_d, 'EUR').cents
-    invoice = Invoice.by_top_up_request(user: current_user, cents: amount)
-    if invoice.save && invoice.reload
-      payment_order = create_payment_order_from_invoice!(invoice)
+    if @invoice.save && @invoice.reload
+      payment_order = create_payment_order_from_invoice!(@invoice)
       if Feature.billing_system_integration_enabled?
-        send_invoice_to_billing_system(invoice)
-        redirect_url = URI.parse(invoice.payment_link).to_s
+        send_invoice_to_billing_system(@invoice)
+        redirect_url = URI.parse(@invoice.payment_link).to_s
       else
         redirect_url = URI.parse(payment_order.linkpay_url).to_s
       end
@@ -25,7 +25,7 @@ class InvoicesController < ApplicationController
         format.json { render :show, status: :created, location: payment_order }
       end
     else
-      redirect_to(invoices_path, notice: invoice.errors)
+      redirect_to(invoices_path, notice: @invoice.errors)
     end
   end
   # rubocop:enable Metrics/MethodLength
@@ -34,13 +34,9 @@ class InvoicesController < ApplicationController
   def update; end
 
   # GET /invoices/aa450f1a-45e2-4f22-b2c3-f5f46b5f906b
-  def show
-    @invoice = current_user.invoices.find_by(uuid: params[:uuid])
-  end
+  def show; end
 
   def download
-    @invoice = current_user.invoices.find_by(uuid: params[:uuid])
-
     pdf = PDFKit.new(render_to_string('common/pdf', layout: false))
     raw_pdf = pdf.to_pdf
 
@@ -48,6 +44,15 @@ class InvoicesController < ApplicationController
   end
 
   private
+
+  def find_invoice
+    @invoice = current_user.invoices.find_by(uuid: params[:uuid])
+  end
+
+  def set_invoice
+    amount = Money.from_amount(params[:amount].to_d, 'EUR').cents
+    @invoice = Invoice.by_top_up_request(user: current_user, cents: amount)
+  end
 
   def create_payment_order_from_invoice!(invoice)
     payment_order = PaymentOrder.new_from_invoice(invoice)
